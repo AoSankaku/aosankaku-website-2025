35a23d68118e12979d2df21d6efe3779f21e8987
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2025-12-16 00:29:32 +0900

    Update to include trailing slash to url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from './src/i18n/locales';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon()],
+  trailingSlash: 'always',
   i18n: {
     defaultLocale: DEFAULT_LOCALE_SETTING,
     locales: Object.keys(LOCALES_SETTING),
@@ -26,5 +27,5 @@ export default defineConfig({
         ignored: ['**/node_modules/**', '**/.git/**'],
       }
     },
-  }
+  },
 });
