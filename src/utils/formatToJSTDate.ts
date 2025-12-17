3da23d53e9ccd1b748e723e029c3f9348cc2ea35
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2025-12-18 00:50:08 +0900

    Update formatToJSTDate function to return future fixed value when invalid value is passed

--- src/utils/formatToJSTDate.ts
+++ src/utils/formatToJSTDate.ts
@@ -1,5 +1,9 @@
 const formatToJSTDate = (date: string) => {
   const now = new Date(date);
+  if (isNaN(now.getTime())) {
+    console.error("[ERROR] Invalid date passed, received", date)
+    return "2100/01/01";
+  }
   const formatter = new Intl.DateTimeFormat("ja-JP", {
     timeZone: "Asia/Tokyo",
     year: "numeric",
