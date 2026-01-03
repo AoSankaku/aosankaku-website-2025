012a168b384dad9723b43a249c63f7be695657ca
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2026-01-04 03:59:01 +0900

    Extract getArticleImage.ts

---
+++ src/utils/getArticleImage.ts
@@ -0,0 +1,16 @@
+import type { ImageMetadata } from "astro";
+import ogDefaultImage from "@/assets/og-default.png";
+
+const images = import.meta.glob<{ default: ImageMetadata }>(
+  "/src/content/blog/**/*.{jpeg,jpg,png,gif}",
+  { eager: true }
+);
+
+export default function getArticleImage(slug: string, thumbnail: string | undefined) {
+  const path = `/src/content/blog/${slug
+    .replace(/^\/blog\//, '')
+    .replace(/\/$/, '')
+    }/${thumbnail ? thumbnail.replace(/^\.\//, "") : "null"}`;
+  console.log(path)
+  return images[path]?.default ?? ogDefaultImage;
+}
