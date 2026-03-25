76bf6f0c97e93ad2d70ecea7a3da2286d917cbb6
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2026-03-25 23:13:39 +0900

    Migrate to Astro 6

--- config.ts
+++ config.ts
@@ -1,5 +1,6 @@
 // @/content/config.ts
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
 import { glob } from 'astro/loaders';
 
 // Helper function to force JST if no timezone is provided
